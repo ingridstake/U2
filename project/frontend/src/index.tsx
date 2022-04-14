78d6aa5b5f8599fc7c00da8fba77d14f4fb6c0b0
Toveenilssonn <tovnil@hotmail.se>
2022-04-14 11:08:07 +0200

    User-story-1.4

--- project/frontend/src/index.tsx
+++ project/frontend/src/index.tsx
@@ -2,13 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import EventList from './show_event';
+import JsonDataDisplay from './show_event';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
-    <EventList />
+    <JsonDataDisplay />
   </React.StrictMode>,
   document.getElementById('root')
 );
--- project/frontend/src/show_event.tsx
+++ project/frontend/src/show_event.tsx
@@ -1,13 +1,51 @@
 import React from "react";
+import test from "./test.json";
+import './show_events.css';
+import Table from "react-bootstrap/Table";
 
+/*
 function EventList() {
-    const events = ['Beertasting', 'Mares', 'Gasquen'];
-    const listItems = events.map((event) =>
+    const listItems = test.map((event) =>
     <li>{event}</li>
     );
     return (
         <ul>{listItems}</ul>
-    )
+    );
 }
+*/
 
-export default EventList;
+function JsonDataDisplay(){
+    const DisplayData=test.map(
+        (test)=>{
+            return(
+                <tr>
+                    <td>{test.id}</td>
+                    <td>{test.name}</td>
+                    <td>{test.city}</td>
+                </tr>
+            )
+        }
+    )
+ 
+    return(
+        <>
+        <Table striped bordered hover>
+                <thead>
+                    <tr>
+                    <th>Id</th>
+                    <th>Name</th>
+                    <th>City</th>
+                    </tr>
+                </thead>
+                <tbody>
+                 
+                    {DisplayData}
+                    
+                </tbody>
+        </Table> 
+        
+        </>
+    );
+ }
+
+export default JsonDataDisplay;
