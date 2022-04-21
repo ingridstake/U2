143ff569c87e5e107014d8a9c944893dd1f92d74
olimanstrom <olivia@manstrom.se>
2022-04-21 17:42:12 +0200

    started user story

--- project/frontend/src/components/ShowEvents.tsx
+++ project/frontend/src/components/ShowEvents.tsx
@@ -1,16 +1,65 @@
-import React from "react";
-import test from "../resources/test.json";
+import React, { useEffect, useState } from 'react';
+import events from "../resources/test.json";
 import '../styles/showEvents.css';
 import Table from "react-bootstrap/Table";
+import axios from 'axios';
+
 
 function JsonDataDisplay() {
-    const DisplayData=test.map(
-        (test)=>{
+
+    const [appState, setAppState] = useState({
+        
+        loading: false,
+        events: [] as any[],
+      });
+    
+    /*
+    useEffect(() => {
+        //setAppState({ loading: true });
+        const apiUrl = 'http://127.0.0.1:8080/ten_events_string';
+        axios.get(apiUrl).then((events) => {
+          const allEvents = events.data;
+          //setAppState({ loading: false, events: allEvents });
+        });
+      });           // [setAppState]
+      */
+    
+    useEffect(() => { 
+        setAppState({ loading: true, events: [] });
+        let allEvents = [
+            {
+            id: '',
+            name: '',
+            city: '',
+            },]
+        
+        const apiUrl = 'http://127.0.0.1:8080/ten_events_string';
+        axios.get(apiUrl).then(res => {
+            const newItem = {
+              id: res.data.id,
+              name: res.data.name,
+              city: res.data.city,
+            };
+            allEvents.push(newItem);
+         })
+        
+        /*.then((events) => {
+          const allEvents = events.data;
+          console.log(allEvents);
+          */
+          setAppState({ loading: false, events: allEvents });
+        
+        //});
+       
+    }, [setAppState]);
+
+    const DisplayData=appState.events.map(
+        (events)=>{
             return(
-                <tr key={test.id}>
-                    <td>{test.id}</td>
-                    <td>{test.name}</td>
-                    <td>{test.city}</td>
+                <tr key={events.id}>
+                    <td>{events.id}</td>
+                    <td>{events.name}</td>
+                    <td>{events.city}</td>
                 </tr>
             )
         }
