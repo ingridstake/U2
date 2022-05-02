437b789b6b4b8aea68224470669c3e50fe9da29f
elvinafahlgren <elvina.fahlgren@live.com>
2022-05-02 16:22:27 +0200

    comments added

--- project/backend/src/main/java/com/U2/backend/EventFiltering/EasyCategorization.java
+++ project/backend/src/main/java/com/U2/backend/EventFiltering/EasyCategorization.java
@@ -1,26 +1,21 @@
 package com.U2.backend.EventFiltering;
 
-import com.U2.backend.APIDataService;
 import com.U2.backend.DataObjectContracts.IEvent;
-import com.U2.backend.DataObjectContracts.IVenue;
 import com.U2.backend.DataObjectFactories.DataObjectFactory;
-import org.json.JSONArray;
 import org.json.JSONException;
-import org.json.JSONObject;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
 public class EasyCategorization {
 
-    public EasyCategorization() {
-
-    }
-
+    /**
+     * Categorizes a list of IEvents into vowels and consonants.
+     * @param events is a list of IEvents.
+     * @return a String of a JSONArray containing categories with events.
+     */
     public static String getCategorization(List<IEvent> events){
         var categoryMap = new HashMap<String,List<IEvent>>();
-
         categoryMap.put("vowels", getVowelEvents(events));
         categoryMap.put("consonants", getConsonantsEvents(events));
         String s = null;
@@ -32,6 +27,11 @@ public class EasyCategorization {
         return s;
     }
 
+    /**
+     * Filters a List of IEvents by the first letter of their name (only vowels).
+     * @param events is the List of IEvents to be filtered
+     * @return a Lists of IEvents which name starts with a vowels
+     */
     private static List<IEvent> getVowelEvents(List<IEvent> events) {
         System.out.println("test");
         String vowels = "aeiouåäö";
@@ -40,6 +40,11 @@ public class EasyCategorization {
         return t;
     }
 
+    /**
+     * Filters a List of IEvents by the first letter of their name (only consonants).
+     * @param events is the List of IEvents to be filtered
+     * @return a Lists of IEvents which name starts with a consonants
+     */
     private static List<IEvent> getConsonantsEvents(List<IEvent> events) {
         System.out.println("test");
         String consonants = "qwrtpsdfghjklzxcvbnm";
