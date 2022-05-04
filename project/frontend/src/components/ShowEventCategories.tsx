f71ca344bc0aeac0b070e97be07a0cb4f9f34b48
elvinafahlgren <elvina.fahlgren@live.com>
2022-05-04 16:39:03 +0200

    css adjustments
    Co-Authored-By: Julia Ekeblad <julia.ekeblad@gmail.com>
    Co-Authored-By: Ingrid Stake <ingrid.stake01@gmail.com>

--- project/frontend/src/components/ShowEventCategories.tsx
+++ project/frontend/src/components/ShowEventCategories.tsx
@@ -11,8 +11,13 @@ import '../styles/showEventCat.css';
 
 
 const responsive = {
-  desktop: {
-    breakpoint: { max: 3000, min: 1024 },
+  desktop1: {
+    breakpoint: { max: 3000, min: 2012 },
+    items: 4,
+    paritialVisibilityGutter: 60
+  },
+  desktop2: {
+    breakpoint: { max: 2012, min: 1024 },
     items: 3,
     paritialVisibilityGutter: 60
   },
@@ -27,23 +32,11 @@ const responsive = {
     paritialVisibilityGutter: 30
   }
 };
-const images = [
-  "https://images.unsplash.com/photo-1549989476-69a92fa57c36?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
-  "https://images.unsplash.com/photo-1549396535-c11d5c55b9df?ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60",
-  "https://images.unsplash.com/photo-1550133730-695473e544be?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
-  "https://images.unsplash.com/photo-1550167164-1b67c2be3973?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
-  "https://images.unsplash.com/photo-1550338861-b7cfeaf8ffd8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
-  "https://images.unsplash.com/photo-1550223640-23097fc71cb2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
-  "https://images.unsplash.com/photo-1550353175-a3611868086b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
-  "https://images.unsplash.com/photo-1550330039-a54e15ed9d33?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
-  "https://images.unsplash.com/photo-1549737328-8b9f3252b927?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
-  "https://images.unsplash.com/photo-1549833284-6a7df91c1f65?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
-  "https://images.unsplash.com/photo-1549985908-597a09ef0a7c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
-  "https://images.unsplash.com/photo-1550064824-8f993041ffd3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60"
-];
 
-//const cat = [];
-
+/**
+ * 
+ * @returns a list of categories with event cards
+ */
 export default function DataCat() {
   const [appState, setAppState] = useState({
     loading: false,
@@ -62,14 +55,13 @@ export default function DataCat() {
   return (
     <ListGroup>
       {appState.cat.map(c => (
-        <ListGroupItem><h1>{c.category}</h1>
+        <ListGroupItem><h1 className="cat-title">{c.category}</h1>
           <Carousel
             //ssr
-            partialVisbile
+            //partialVisbile
             /*deviceType={deviceType}*/
             //itemClass="image-item"
             responsive={responsive} 
-            className="carousel"
           >
           {c.events.map((e: { imageUrl: string; name: string; date: string; city: string; }) => {
               return (
@@ -91,52 +83,8 @@ export default function DataCat() {
         ))}
     </ListGroup>
   );
-
 }
 
 
 // Because this is an inframe, so the SSR mode doesn't not do well here.
 // It will work on real devices.
-/*
-const Simple = ({  }) => {
-  return (
-    <Carousel
-      ssr
-      partialVisbile
-      deviceType={deviceType}
-      itemClass="image-item"
-      responsive={responsive}
-    >
-      {images.slice(0, 5).map(image => {
-        return (
-          <Image
-            draggable={false}
-            style={{ width: "100%", height: "100%" }}
-            src={image}
-          />
-        );
-      })}
-    </Carousel>
-  );
-};
-
-export default Simple;
-*/
-
-/*
-          {c.event.map((e: { imageUrl: string | undefined; }) => {
-            return (
-              <Card>
-                  <Card.Body className='c-body'>
-                      <Card.Img variant="top" src={e.imageUrl} />
-                      <Card.Title className='c-title'><h1>{"hello"}</h1> </Card.Title>
-                  </Card.Body>
-                  
-                  <Card.Footer className='c-footer'>
-                      <Button>Köp</Button>
-                      <Card.Text><p>{"hej"}, {"hola"}</p></Card.Text>
-                  </Card.Footer>
-              </Card>
-            );
-          })}
-          */
