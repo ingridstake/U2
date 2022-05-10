61ca116ade174d0115d2a184e576ba0e13b6b5d2
ingridstake <ingrid.stake01@gmail.com>
2022-05-10 11:23:43 +0200

    adjusts the number of cards one sees depending on screen size
    Co-Authored-By: juliaeke <74048004+juliaeke@users.noreply.github.com>
    Co-Authored-By: elvinafahlgren <70009525+elvinafahlgren@users.noreply.github.com>

--- project/frontend/src/components/ShowEventCategories.tsx
+++ project/frontend/src/components/ShowEventCategories.tsx
@@ -12,11 +12,11 @@ import { category } from './Models';
  */
 const responsive = {
   desktopBig: {
-    breakpoint: { max: 3000, min: 1520 },
+    breakpoint: { max: 3000, min: 1550 },
     items: 4,
   },
   desktopSmall: {
-    breakpoint: { max: 1520, min: 768 },
+    breakpoint: { max: 1550, min: 768 },
     items: 3,
   },
   tablet: {
