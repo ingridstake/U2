d9a5203e0e11270e57a73d80cbbea2d844793a49
olimanstrom <olivia@manstrom.se>
2022-05-11 16:10:19 +0200

    frontend takes in a parameter that is sent to backend, that sends back the result
    co-authored-by: Tove Nilsson <toveni@student.chalmers.se>

--- project/frontend/src/components/SearchBar.tsx
+++ project/frontend/src/components/SearchBar.tsx
@@ -2,52 +2,31 @@ import {useEffect, useState } from "react";
 import '../styles/searchBar.css';
 import axios from 'axios';
 
+/**
+ * Search for the search parameter and returns a string of matching events
+ * @returns a string of matching events
+ * getInputValue recives the input from the search bar
+ * startSearch sends the search parameter to the backend and return the matching events
+ */
 export default function SearchBar() {
-
-    // working!!  Nu funkasr det
-    /*
-    useEffect(()=> {
-        fetch('http://127.0.0.1:8080/search').then(resp=>resp.text())
-        .then(resp => {
-            console.log('eventsting from api: ' + resp)
-        })
-    }, [])
-    */
-    const [appState, setAppState] = useState({
-        loading: false,
-        result: String as any,
-        searchParam: String as any
-    });
     
-    const getInputValue = (event: { target: { value: any; }; })=>{
-    // show the user input value to console
-        const userValue = event.target.value;
+    const [searchParam, setSearchParam] = useState('');
 
-        console.log(userValue);
-    }; 
+    const getInputValue = (e: { target: { value: any; }; }) => {
+        const userVal = e.target.value;
+        setSearchParam(userVal)        
+        console.log(searchParam)
+    }
       
-    useEffect(() => {
-        setAppState({loading: true, result: "", searchParam: getInputValue});
-        // const apiUrl ='http://127.0.0.1:8080/search?param';
-        
-        axios.get('http://localhost:8080/search?param=p' ).then(res => {
+    const startSearch = 
+        axios.get('http://localhost:8080/search?param='+searchParam).then(res => {
             const allResults = res.data
             console.log(allResults)
-            setAppState({ loading: false, result: allResults, searchParam: getInputValue});
         })
-    }, [setAppState])
-   
+
     return (
     <div className="SearchBar">
         <input className="InputText" type="search" placeholder="Sökord..." onChange={getInputValue}  />
     </div>
     );
-
-     /*
-    return (
-        <div className="SearchBar">
-            <input className="InputText" type="search" placeholder="Sökord..." />
-        </div>
-        );
-        */
 }
