5ef75a18e04ef5980cd7a0869fa95bffdd60fc99
Toveenilssonn <tovnil@hotmail.se>
2022-05-11 14:48:52 +0200

    started user 3.2
    co-authored-by: Olivia Månström <olivia@manstrom.se>

--- project/backend/src/main/java/com/U2/backend/BackendApplication.java
+++ project/backend/src/main/java/com/U2/backend/BackendApplication.java
@@ -39,7 +39,7 @@ public class BackendApplication {
 	@GetMapping(path = "/search")
 	public String search(@RequestParam(value = "param", defaultValue = "") String param) {
 		var service = ServiceFactory.createSearchService(events);
-		return service.performSearch("p");
+		return service.performSearch(param);
 	}
 
 	@CrossOrigin
--- project/frontend/src/components/SearchBar.tsx
+++ project/frontend/src/components/SearchBar.tsx
@@ -15,28 +15,39 @@ export default function SearchBar() {
     */
     const [appState, setAppState] = useState({
         loading: false,
-        result: String as any
+        result: String as any,
+        searchParam: String as any
     });
+    
+    const getInputValue = (event: { target: { value: any; }; })=>{
+    // show the user input value to console
+        const userValue = event.target.value;
 
+        console.log(userValue);
+    }; 
+      
     useEffect(() => {
-        setAppState({loading: true, result: ""});
-        const apiUrl ='http://127.0.0.1:8080/search';
-        axios.get(apiUrl).then(res => {
+        setAppState({loading: true, result: "", searchParam: getInputValue});
+        // const apiUrl ='http://127.0.0.1:8080/search?param';
+        
+        axios.get('http://localhost:8080/search?param=p' ).then(res => {
             const allResults = res.data
             console.log(allResults)
-            setAppState({ loading: false, result: allResults});
+            setAppState({ loading: false, result: allResults, searchParam: getInputValue});
         })
     }, [setAppState])
-    /*
+   
     return (
     <div className="SearchBar">
         <input className="InputText" type="search" placeholder="Sökord..." onChange={getInputValue}  />
     </div>
     );
-    */
+
+     /*
     return (
         <div className="SearchBar">
             <input className="InputText" type="search" placeholder="Sökord..." />
         </div>
         );
+        */
 }
